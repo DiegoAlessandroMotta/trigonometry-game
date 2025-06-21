a4e15dba623cecde441501f95ed7bb5d9e1cf3e1
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-06-21 11:05:31 -0500

    Refactor triggers and functions: rename update function and triggers for clarity; ensure updated_at timestamp is set correctly

--- supabase/functions.sql
+++ supabase/functions.sql
@@ -1,13 +1,46 @@
--- Trigger to update updated_at timestamp
-create
-or
-replace
-  function public.update_updated_at_column () returns trigger language plpgsql security definer
-set
-  search_path = "" as $$ begin new.updated_at = current_timestamp;
+create or replace 
+function public.fn_update_updated_at_column () 
+returns trigger 
+language plpgsql security definer set search_path = "" as $$ 
+begin 
+  new.updated_at = current_timestamp;
 
-return new;
-
+  return new;
 end;
+$$;
 
+create or replace 
+function public.fn_update_puntos_totales () 
+returns trigger 
+language plpgsql security definer set search_path = "" as $$ 
+declare 
+  puntos_a_sumar int;
+  id_estudiante uuid;
+begin 
+  if new.respuesta_correcta = true then
+    select
+      puntos into puntos_a_sumar
+    from
+      public.preguntas
+    where
+      id = new.pregunta_id;
+
+    select
+      estudiante_id into id_estudiante
+    from
+      public.sesiones_interactivas
+    where
+      id = new.sesion_interactiva_id;
+
+    if puntos_a_sumar is not null and id_estudiante is not null then
+      update public.estudiantes
+      set
+        puntos_totales = puntos_totales + puntos_a_sumar
+      where
+        id = id_estudiante;
+    end if;
+  end if;
+
+  return null;
+end;
 $$;
--- supabase/triggers.sql
+++ supabase/triggers.sql
@@ -1,11 +1,19 @@
-create trigger update_estudiantes_updated_at before
+create trigger trg_update_estudiantes_updated_at before
 update on public.estudiantes for each row
 execute function public.update_updated_at_column ();
 
-create trigger update_minijuegos_updated_at before
+create trigger trg_update_minijuegos_updated_at before
 update on public.minijuegos for each row
 execute function public.update_updated_at_column ();
 
-create trigger update_sesiones_minijuego_updated_at before
+create trigger trg_update_sesiones_minijuego_updated_at before
 update on public.sesiones_minijuego for each row
 execute function public.update_updated_at_column ();
+
+create trigger trg_update_puntos_evaluacion after
+insert on public.respuestas_evaluacion for each row 
+execute function public.actualizar_puntos_estudiante ();
+
+create trigger trg_update_puntos_minijuego after
+insert on public.respuestas_minijuego for each row 
+execute function public.actualizar_puntos_estudiante ();
