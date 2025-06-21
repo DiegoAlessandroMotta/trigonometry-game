9609377fcc49e4c38d6c0455a51da7604b71e524
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-06-20 21:55:01 -0500

    Refactor database schema: update enum type, rename tables, and adjust foreign key references

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -1,12 +1,11 @@
-create type estudiantes_genero as enum('M', 'F');
+create type tipo_genero_estudiante as enum('M', 'F');
 
 create table
   public.estudiantes (
-    id uuid primary key default gen_random_uuid (),
-    auth_id uuid unique references auth.users (id) on delete cascade,
+    id uuid primary key unique references auth.users (id) on delete cascade,
     nombre_usuario text not null unique,
     edad integer check (edad > 0),
-    genero estudiantes_genero,
+    genero tipo_genero_estudiante,
     es_anonimo boolean not null default true,
     puntos_totales integer not null default 0,
     created_at timestamp
@@ -20,6 +19,7 @@ create table
 create table
   public.minijuegos (
     id uuid primary key default gen_random_uuid (),
+    tema text not null,
     codigo text not null unique,
     nombre text not null unique,
     created_at timestamp
@@ -31,9 +31,10 @@ create table
   );
 
 create table
-  public.preguntas_prueba (
+  public.preguntas (
     id uuid primary key default gen_random_uuid (),
     codigo text not null,
+    tema text not null,
     titulo text not null,
     enunciado text not null,
     puntos integer not null,
@@ -46,6 +47,7 @@ create table
           bool_and (
             (value ->> 'texto' is not null)
             and (value ->> 'esCorrecta' is not null)
+            and (value ->> 'feedback' is not null)
           )
         from
           jsonb_array_elements (alternativas)
@@ -57,36 +59,50 @@ create table
   public.sesiones_interactivas (
     id uuid primary key default gen_random_uuid (),
     estudiante_id uuid references public.estudiantes (id) on delete set null,
-    puntuacion_prueba integer not null check (puntuacion_prueba >= 0),
     created_at timestamp
     with
       time zone default current_timestamp not null
   );
 
 create table
-  public.preguntas_minijuego (
+  public.respuestas_evaluacion (
     id uuid primary key default gen_random_uuid (),
-    minijuego_id uuid not null,
-    sesion_interactiva_id uuid not null,
-    pregunta_prueba_id uuid not null,
+    sesion_interactiva_id uuid not null references public.sesiones_interactivas (id) on delete cascade,
+    pregunta_id uuid references public.preguntas (id) on delete set null,
+    respuesta_correcta boolean not null default false,
+    respuesta_dada text,
+    tiempo_respuesta int not null,
     created_at timestamp
     with
-      time zone default current_timestamp not null,
-      foreign key (minijuego_id) references public.minijuegos (id),
-      foreign key (sesion_interactiva_id) references public.sesiones_interactivas (id),
-      foreign key (pregunta_prueba_id) references public.preguntas_prueba (id)
+      time zone default current_timestamp not null
   );
 
 create table
-  public.evaluacion_minijuego (
+  public.respuestas_minijuego (
     id uuid primary key default gen_random_uuid (),
-    minijuego_id uuid not null,
-    sesion_interactiva_id uuid not null,
+    minijuego_id uuid not null references public.minijuegos (id),
+    sesion_interactiva_id uuid not null references public.sesiones_interactivas (id) on delete cascade,
+    pregunta_id uuid references public.preguntas (id) on delete set null,
+    respuesta_correcta boolean not null default false,
+    tiempo_respuesta int not null,
+    respuesta_dada text,
+    created_at timestamp
+    with
+      time zone default current_timestamp not null
+  );
+
+create table
+  public.sesiones_minijuego (
+    id uuid primary key default gen_random_uuid (),
+    minijuego_id uuid not null references public.minijuegos (id) on delete cascade,
+    sesion_interactiva_id uuid not null references public.sesiones_interactivas (id) on delete cascade,
+    intentos integer not null default 1,
     inicio timestamp not null,
     final timestamp not null,
     created_at timestamp
     with
       time zone default current_timestamp not null,
-      foreign key (minijuego_id) references public.minijuegos (id),
-      foreign key (sesion_interactiva_id) references public.sesiones_interactivas (id)
+      updated_at timestamp
+    with
+      time zone default current_timestamp not null
   );
