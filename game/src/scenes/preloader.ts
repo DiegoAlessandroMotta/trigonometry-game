1054e75bbb2819cfd6785c010d2558f8f1991512
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-07-06 14:24:13 -0500

    fix: preload level 1

--- game/src/scenes/preloader.ts
+++ game/src/scenes/preloader.ts
@@ -49,7 +49,7 @@ export class Preloader extends Phaser.Scene {
 
   loadTileAssets() {
     this.load.setPath('assets/tiles')
-    this.load.tilemapTiledJSON('level1', 'level2.json')
+    this.load.tilemapTiledJSON('level1', 'level1.json')
 
     this.load.atlas('tiles', 'tileset.png', 'tileset.json')
     this.load.atlas('gems', 'gems-tileset.png', 'gems-tileset.json')
