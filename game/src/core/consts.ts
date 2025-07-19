fc27bcb866b6e306af39cd6fbf7098275cb0bef7
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-07-19 12:26:02 -0500

    define more player animation names

--- game/src/core/consts.ts
+++ game/src/core/consts.ts
@@ -2,7 +2,15 @@ export const pixelFont = 'pixel-emulator'
 
 export const animationsNames = {
   player: {
+    stopped: 'player-stopped',
+    idle: 'player-idle',
+    run: 'player-run',
+    jump: 'player-jump',
+    airJump: 'player-air-jump',
+    wallSlide: 'player-wall-slide',
+    fall: 'player-fall',
     appearing: 'player-appearing',
-    disappearing: 'player-disappearing'
+    disappearing: 'player-disappearing',
+    doubleJump: 'player-double-jump'
   }
 }
