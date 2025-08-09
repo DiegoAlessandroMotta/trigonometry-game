c82f499ecec56526ff3d1261aea167d8039cc35c
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-08-09 15:48:27 -0500

    feat: implement pagination in dialog scene

--- game/src/scenes/dialog.ts
+++ game/src/scenes/dialog.ts
@@ -1,17 +1,109 @@
-import { fonts, scenes } from '@/core/consts'
+import { customEvents, fonts, scenes } from '@/core/consts'
+
+interface DialogPage {
+  title: string
+  textContent: string
+  illustrations: {
+    texture: string
+    animationKey?: string
+    frame?: string
+    width?: number
+    height?: number
+    scale?: number
+  }[]
+}
+
+interface DialogProps {
+  width: number
+  height: number
+  pages: DialogPage[]
+  initialPage?: number
+}
+
+interface Position {
+  x: number
+  y: number
+}
 
 export class DialogScene extends Phaser.Scene {
+  private position: Position = { x: 0, y: 0 }
+  private width: number
+  private height: number
+  private pages: DialogPage[]
+  private currentPage: number
+
+  private pageContentContainer!: Phaser.GameObjects.Container
+  private nextButton!: Phaser.GameObjects.Sprite
+  private nextButtonIcon!: Phaser.GameObjects.Sprite
+  private prevButton!: Phaser.GameObjects.Sprite
+  private prevButtonIcon!: Phaser.GameObjects.Sprite
+
+  private textureTheme = 'green'
+  private buttonsTextureTheme = {
+    normal: 'blue-square.png',
+    pressed: 'blue-square-pressed.png'
+  }
+  private nextButtonIconTexture = 'icon-arrow-right.png'
+  private prevButtonIconTexture = 'icon-arrow-left.png'
+  private closeButtonIconTexture = 'icon-x-mark.png'
+  private backgroundScale = 2
+  private padding = 8
+  private iconButtonYOffset = 4
+
   constructor() {
     super(scenes.dialog)
+
+    this.width = 0
+    this.height = 0
+    this.pages = []
+    this.currentPage = 0
+  }
+
+  public init({ width, height, pages, initialPage }: DialogProps) {
+    this.width = width ?? 16 * 32
+    this.height = height ?? 16 * 8
+    this.pages = pages ?? []
+    this.currentPage = initialPage ?? 0
+
+    this.position.x = (this.cameras.main.width - this.width) / 2
+    this.position.y =
+      (this.cameras.main.height - this.height) / 2 - this.backgroundScale * 8
   }
 
   public create() {
-    this.drawDialog()
+    this.game.events.emit(customEvents.pauseGame)
+
+    this.add
+      .rectangle(
+        0,
+        0,
+        this.cameras.main.width,
+        this.cameras.main.height,
+        0x000000,
+        0.7
+      )
+      .setInteractive()
+      .setOrigin(0)
+
+    this.drawDialogBackground()
+    this.drawNavigationButtons()
+
+    const dialogInnerX = this.position.x
+    const dialogInnerY = this.position.y
+    const dialogInnerWidth = this.width
+    const dialogInnerHeight = this.height
+
+    this.pageContentContainer = this.add.container(
+      dialogInnerX + dialogInnerWidth / 2,
+      dialogInnerY + dialogInnerHeight / 2
+    )
+
+    this.renderPageContent()
+    this.updateNavigationButtons()
   }
 
-  private drawDialog() {
-    const dialogTextureTheme = 'green'
-
+  private drawDialogBackground() {
+    const dialogTextureTheme = this.textureTheme
     const tileFrames = {
       topLeft: `ui-${dialogTextureTheme}-1.png`,
       top: `ui-${dialogTextureTheme}-2.png`,
@@ -23,35 +115,31 @@ export class DialogScene extends Phaser.Scene {
       bottom: `ui-${dialogTextureTheme}-8.png`,
       bottomRight: `ui-${dialogTextureTheme}-9.png`
     }
-
     const tileBaseSize = 16
 
-    const boxPosition = {
-      x: tileBaseSize * 4,
-      y: tileBaseSize * -1.5
-    }
-
     const boxConfig = {
-      x: boxPosition.x + tileBaseSize * 2,
-      y: boxPosition.y + tileBaseSize * 2,
-      width: tileBaseSize * 37,
-      height: tileBaseSize * 2.8
+      x: this.position.x + tileBaseSize,
+      y: this.position.y + tileBaseSize,
+      width: this.width,
+      height: this.height
     }
 
     this.add
       .tileSprite(
         boxConfig.x,
         boxConfig.y,
-        boxConfig.width,
-        boxConfig.height,
+        boxConfig.width / this.backgroundScale,
+        boxConfig.height / this.backgroundScale,
         'gui-tileset',
         tileFrames.center
       )
       .setOrigin(0)
+      .setScale(this.backgroundScale)
 
     this.add
       .sprite(boxConfig.x, boxConfig.y, 'gui-tileset', tileFrames.topLeft)
       .setOrigin(1, 1)
+      .setScale(this.backgroundScale)
 
     this.add
       .sprite(
@@ -61,6 +149,7 @@ export class DialogScene extends Phaser.Scene {
         tileFrames.topRight
       )
       .setOrigin(0, 1)
+      .setScale(this.backgroundScale)
 
     this.add
       .sprite(
@@ -70,6 +159,7 @@ export class DialogScene extends Phaser.Scene {
         tileFrames.bottomLeft
       )
       .setOrigin(1, 0)
+      .setScale(this.backgroundScale)
 
     this.add
       .sprite(
@@ -79,107 +169,259 @@ export class DialogScene extends Phaser.Scene {
         tileFrames.bottomRight
       )
       .setOrigin(0, 0)
+      .setScale(this.backgroundScale)
 
     this.add
       .tileSprite(
         boxConfig.x,
         boxConfig.y,
-        boxConfig.width,
+        boxConfig.width / this.backgroundScale,
         tileBaseSize,
         'gui-tileset',
         tileFrames.top
       )
       .setOrigin(0, 1)
+      .setScale(this.backgroundScale)
 
     this.add
       .tileSprite(
         boxConfig.x,
         boxConfig.y,
         tileBaseSize,
-        boxConfig.height,
+        boxConfig.height / this.backgroundScale,
         'gui-tileset',
         tileFrames.left
       )
       .setOrigin(1, 0)
+      .setScale(this.backgroundScale)
 
     this.add
       .tileSprite(
         boxConfig.x + boxConfig.width,
         boxConfig.y,
         tileBaseSize,
-        boxConfig.height,
+        boxConfig.height / this.backgroundScale,
         'gui-tileset',
         tileFrames.right
       )
       .setOrigin(0)
+      .setScale(this.backgroundScale)
 
     this.add
       .tileSprite(
         boxConfig.x,
         boxConfig.y + boxConfig.height,
-        boxConfig.width,
+        boxConfig.width / this.backgroundScale,
         tileBaseSize,
         'gui-tileset',
         tileFrames.bottom
       )
       .setOrigin(0, 0)
+      .setScale(this.backgroundScale)
+  }
 
-    // Render content
-    this.add.bitmapText(
-      boxPosition.x + tileBaseSize * 4,
-      boxPosition.y + tileBaseSize * 2,
-      fonts.pixel,
-      'Los triángulos equiláteros son aquellos que tienen todos sus lados \niguales.\nRecolecta todos los triángulos equiláteros'
-    )
+  private renderPageContent() {
+    this.pageContentContainer.removeAll(true)
 
-    this.add
-      .sprite(
-        boxPosition.x + tileBaseSize * 2.5,
-        boxPosition.y + tileBaseSize * 3,
-        'objects',
-        'equilatero-1.png'
+    if (this.currentPage >= this.pages.length) {
+      console.warn('Attempted to render a page out of bounds.')
+      return
+    }
+
+    const currentPageData = this.pages[this.currentPage]
+
+    const padding = this.padding
+    const contentAreaWidth = this.width - padding * 2
+    const contentAreaHeight = this.height - padding * 2
+
+    const contentTopLeftX = -contentAreaWidth / 2
+    const contentTopLeftY = -contentAreaHeight / 2
+
+    const titleText = this.add
+      .bitmapText(
+        contentTopLeftX + padding / 2,
+        contentTopLeftY + padding / 2,
+        fonts.pixel,
+        currentPageData.title
       )
+      .setOrigin(0)
+      .setScale(2)
+    this.pageContentContainer.add(titleText)
+
+    const textContent = this.add
+      .bitmapText(
+        contentTopLeftX + padding / 2,
+        titleText.y + titleText.height + padding / 2,
+        fonts.pixel,
+        currentPageData.textContent
+      )
+      .setOrigin(0)
+      .setMaxWidth(contentAreaWidth)
+    this.pageContentContainer.add(textContent)
+
+    let currentIllustrationY = textContent.y + textContent.height + padding
+
+    currentPageData.illustrations.forEach((illustration) => {
+      const illustrationSprite = this.add.image(
+        0,
+        currentIllustrationY,
+        illustration.texture,
+        illustration.frame
+      )
+
+      illustrationSprite.setOrigin(0.5, 0)
+
+      this.pageContentContainer.add(illustrationSprite)
+
+      if (illustration.scale != null) {
+        illustrationSprite.setScale(illustration.scale)
+      } else if (illustration.width && illustration.height) {
+        illustrationSprite.displayWidth = illustration.width
+        illustrationSprite.displayHeight = illustration.height
+      } else if (illustration.width) {
+        illustrationSprite.displayWidth = illustration.width
+        illustrationSprite.scaleY = illustrationSprite.scaleX
+      } else if (illustration.height) {
+        illustrationSprite.displayHeight = illustration.height
+        illustrationSprite.scaleX = illustrationSprite.scaleY
+      }
+
+      currentIllustrationY += illustrationSprite.displayHeight + padding / 2
+    })
+
+    this.updateNavigationButtons()
+  }
+
+  private drawNavigationButtons() {
+    const tileBaseSize = 16
+    const dialogRightEdge = this.position.x + tileBaseSize + this.width
+    const dialogBottomEdge = this.position.y + tileBaseSize + this.height
+
+    this.nextButton = this.add.sprite(
+      dialogRightEdge - tileBaseSize / 2,
+      dialogBottomEdge - tileBaseSize / 2,
+      'buttons-tileset',
+      this.buttonsTextureTheme.normal
+    )
+    this.nextButton
       .setOrigin(0.5)
       .setScale(2)
+      .setInteractive()
+      .setScrollFactor(0)
 
-    // Controls
-    const closeButton = this.add.sprite(
-      boxPosition.x + tileBaseSize * 38,
-      boxPosition.y + tileBaseSize * 4,
-      'buttons-tileset',
-      'orange-square.png'
-    )
-    closeButton.setOrigin(0.5).setScale(2).setInteractive().setScrollFactor(0)
-
-    const iconOffset = 4
-
-    const closeButtonIcon = this.add.sprite(
-      closeButton.x,
-      closeButton.y - iconOffset,
+    this.nextButtonIcon = this.add.sprite(
+      this.nextButton.x,
+      this.nextButton.y - this.iconButtonYOffset,
       'gui-tileset',
-      'icon-x-mark.png'
+      this.nextButtonIconTexture
     )
+    this.nextButtonIcon.setOrigin(0.5).setScrollFactor(0).setScale(2)
 
-    closeButtonIcon.setOrigin(0.5).setScale(2).setScrollFactor(0)
-
-    closeButton.on('pointerdown', () => {
-      closeButton.setTexture('buttons-tileset', 'orange-square-pressed.png')
-      closeButtonIcon?.setY(closeButton.y)
+    this.nextButton.on('pointerdown', () => {
+      this.nextButton.setTexture(
+        'buttons-tileset',
+        this.buttonsTextureTheme.pressed
+      )
+      this.nextButtonIcon.setY(this.nextButton.y)
     })
 
-    closeButton.on('pointerup', () => {
-      closeButton.setTexture('buttons-tileset', 'orange-square.png')
-      closeButtonIcon.setY(closeButton.y - iconOffset)
-      this.handleClickCloseButton()
+    this.nextButton.on('pointerup', () => {
+      this.nextButton.setTexture(
+        'buttons-tileset',
+        this.buttonsTextureTheme.normal
+      )
+      this.nextButtonIcon.setY(this.nextButton.y - this.iconButtonYOffset)
+      this.handleNextPage()
     })
 
-    closeButton.on('pointerout', () => {
-      closeButton.setTexture('buttons-tileset', 'orange-square.png')
-      closeButtonIcon.setY(closeButton.y - iconOffset)
+    this.nextButton.on('pointerout', () => {
+      this.nextButton.setTexture(
+        'buttons-tileset',
+        this.buttonsTextureTheme.normal
+      )
+      this.nextButtonIcon.setY(this.nextButton.y - this.iconButtonYOffset)
+    })
+
+    this.prevButton = this.add.sprite(
+      dialogRightEdge - tileBaseSize * 3,
+      dialogBottomEdge - tileBaseSize / 2,
+      'buttons-tileset',
+      this.buttonsTextureTheme.normal
+    )
+    this.prevButton
+      .setOrigin(0.5)
+      .setScale(2)
+      .setInteractive()
+      .setScrollFactor(0)
+
+    this.prevButtonIcon = this.add.sprite(
+      this.prevButton.x,
+      this.prevButton.y - this.iconButtonYOffset,
+      'gui-tileset',
+      this.prevButtonIconTexture
+    )
+    this.prevButtonIcon.setOrigin(0.5).setScrollFactor(0).setScale(2)
+
+    this.prevButton.on('pointerdown', () => {
+      this.prevButton.setTexture(
+        'buttons-tileset',
+        this.buttonsTextureTheme.pressed
+      )
+      this.prevButtonIcon.setY(this.prevButton.y)
+    })
+
+    this.prevButton.on('pointerup', () => {
+      this.prevButton.setTexture(
+        'buttons-tileset',
+        this.buttonsTextureTheme.normal
+      )
+      this.prevButtonIcon.setY(this.prevButton.y - this.iconButtonYOffset)
+      this.handlePrevPage()
+    })
+
+    this.prevButton.on('pointerout', () => {
+      this.prevButton.setTexture(
+        'buttons-tileset',
+        this.buttonsTextureTheme.normal
+      )
+      this.prevButtonIcon.setY(this.prevButton.y - this.iconButtonYOffset)
     })
   }
 
+  private updateNavigationButtons() {
+    if (this.currentPage === 0) {
+      this.prevButton.disableInteractive().setAlpha(0.5)
+      this.prevButtonIcon.setAlpha(0.5)
+    } else {
+      this.prevButton.setInteractive().setAlpha(1)
+      this.prevButtonIcon.setAlpha(1)
+    }
+
+    if (this.currentPage === this.pages.length - 1) {
+      this.nextButtonIcon.setTexture('gui-tileset', this.closeButtonIconTexture)
+    } else {
+      this.nextButtonIcon.setTexture('gui-tileset', this.nextButtonIconTexture)
+    }
+  }
+
+  private handleNextPage() {
+    if (this.currentPage < this.pages.length - 1) {
+      this.currentPage++
+      this.renderPageContent()
+    } else {
+      this.handleClickCloseButton()
+    }
+  }
+
+  private handlePrevPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--
+      this.renderPageContent()
+    }
+  }
+
   private handleClickCloseButton() {
+    this.game.events.emit(customEvents.pauseGame)
     this.scene.stop()
   }
 }
